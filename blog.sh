95a87492b40f6f765e0c4e83e389caebf63f0b62
OSiRiS <osiris@gcoop.coop>
2020-03-24 05:09:19 -0300

    better regex in get_image of blog.sh
      @ 00:05 hs

--- blog.sh
+++ blog.sh
@@ -33,7 +33,8 @@ get_title()
 
 get_image()
 {
-  grep -Eo 'file:img/.*\.(png|jpg|gif)' "$FILE" \
+  grep -Eo 'file:(tmb|img)/.*\.(png|jpg|gif)' "$FILE" \
+    | sed 's/\]\[/\n/g' \
     | head -1 2>/dev/null
 }
 
