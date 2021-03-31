1fe3bbef2522ee068111449fbc1b1d880bcef62d
OSiRiS <osiux@osiux.com>
2021-03-31 12:46:36 -0300

    agregar generador de /thumbnails/ de imágenes
      @ 00:05 hs

---
+++ tmb.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This script comes with ABSOLUTELY NO WARRANTY, use at own risk
+# Copyright (C) 2021 Osiris Alejandro Gomez <osiux@osiux.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+find img -type f           \
+  | grep -E '\.(png|jpg)$' \
+  | cut -d/ -f2-           \
+  | while read -r IMG
+do
+
+  TMB="tmb/$IMG"
+  [[ -e "$TMB" ]] && continue
+
+  DIR="$(dirname "$IMG")"
+  mkdir -p "tmb/$DIR"
+
+  echo "## convert $IMG to $TMB ..."
+  convert -resize 640x "img/$IMG" "$TMB"
+
+done
