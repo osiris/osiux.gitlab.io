1854acee409ac5b7d3c757082bf08bfe0d10f168
OSiRiS <osiris@gcoop.coop>
2021-11-29 10:39:28 -0300

    eliminar =package-install= de =publish.el=
      @ 00:35 hs

--- publish.el
+++ publish.el
@@ -7,14 +7,6 @@
 
 ;;; Code:
 
-(require 'package)
-(package-initialize)
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-refresh-contents)
-(package-install 'org-plus-contrib)
-(package-install 'htmlize)
-
 (require 'org)
 (require 'ox-publish)
 
