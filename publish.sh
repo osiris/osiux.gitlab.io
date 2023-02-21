6d767a7080a062f8f5d79e9411b5966af980ace1
OSiRiS <osiris@gcoop.coop>
2023-02-21 12:05:43 -0300

    publish: add install package org in publish and remove site-static.cache
      @ 00:01 hs

--- publish.sh
+++ publish.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-rm -f "$HOME/.org-timestamps/site-static.cache"
+CACHE="$HOME/.org-timestamps/site-static.cache"
+[[ -d "$CACHE" ]] && rm -fv "$CACHE"
+emacs --batch --eval "(progn (package-initialize) (package-refresh-contents) (package-install 'org))"
 emacs --batch --no-init-file --load publish.el --funcall org-publish-all
-
