7eb17735e9dd5ded8b5766ed3da00ab5dc644509
OSiRiS <osiris@gcoop.coop>
2023-03-16 15:25:07 -0300

    stats_since_until: show stats for each posts range
      @ 00:50 hs

--- stats_since_until.sh
+++ stats_since_until.sh
@@ -23,8 +23,55 @@ DATES=$(cat << EOF
 EOF
 )
 
-echo "$DATES" | grep -v '\#' | while read -r SINCE UNTIL
+function get_value()
+{
+  grep "$1" "$2" | grep -Eo "$3" | tr -d '=' | sed 's/ \+$//g'
+}
+
+function get_first_hash()
+{
+  get_value '=FIRST_HASH=' "$1" '=[a-f0-9]{7}='
+}
+
+function get_last_hash()
+{
+  get_value '=LAST_HASH=' "$1" '=[a-f0-9]{7}='
+}
+
+function get_time()
+{
+  get_value '=TOTAL_TIME=' "$1" '=[0-9]{2}:[0-9]{2} hs='
+}
+
+function get_commits()
+{
+  get_value '=TOTAL_COMMITS=' "$1" '=[0-9]+='
+}
+
+function get_posts()
+{
+  VALUE="$(get_value '=TOTAL_BLOG_POSTS=' "$1" '=[0-9]+=')"
+  echo "$VALUE"
+}
+
+TMP="$(mktemp)"
+echo "$DATES" | grep -v '\#' > "$TMP"
+
+while read -r SINCE UNTIL
 do
+
   ORG="$SINCE-$UNTIL-posts.org"
-  ./stats-posts-since-until.sh "$SINCE" "$UNTIL" > "$ORG" && echo "$ORG"
-done
+  ./stats-posts-since-until.sh "$SINCE" "$UNTIL" > "$ORG" || continue
+  DATE="$(stat -c %w "$ORG")"
+  printf "%s "         "${DATE%.*}"
+  printf "file=%s "    "$ORG"
+  printf "first=%s "   "$(get_first_hash "$ORG")"
+  printf "last=%s "    "$(get_last_hash "$ORG")"
+  printf "time=%s "    "$(get_time "$ORG")"
+  printf "commits=%s " "$(get_commits "$ORG")"
+  printf "posts=%s "   "$(get_posts "$ORG")"
+  printf "\\n"
+
+done < "$TMP"
+
+rm -f "$TMP"
